184109771a618a8f2fe780d7535166fbbe7a0dc8
KaynnanFelix <felix@felix-windows>
2022-05-08 14:33:28 -0300

    created main

---
+++ main.py
@@ -0,0 +1,37 @@
+from account import Account
+from transaction import Transaction
+from messages import error_message
+from utils import convert_data_entry_to_dictionary_format, is_data_valid, is_empty_line, is_header
+from validationInput import isValid
+
+
+with open('my_program/entrada.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        if (is_header(line)):
+            continue
+
+        if(is_empty_line(line)):
+            continue
+
+        data = line.split('|')
+        if(is_data_valid(data)):
+            result = convert_data_entry_to_dictionary_format(data)
+            if(isValid(result) == False):
+                continue
+
+            sender_account = Account(
+                result['nome_emissor'], result['agencia_emissor'], result['conta_emissor'], result['cpf_emissor'])
+            receiver_account = Account(
+                result['nome_receptor'], result['agencia_receptor'], result['conta_receptor'], result['cpf_receptor'])
+
+            if(sender_account.validate_accounts_for_transfer(receiver_account)):
+                print(error_message(
+                    'não é possível efetuar transferência para a mesma conta'))
+                continue
+
+            transaction = Transaction(
+                result['id_transferencia'], result['valor_transferencia'], result['tipo_transferencia'])
+
+        else:
+            print(error_message(
+                'não foi informado todos os dados necessários para efetuar transação'))
